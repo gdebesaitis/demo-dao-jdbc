43d2f49c6745bb9c423cc812f1a106762c7ea408
gdebesaitis <guilhermedbdev@gmail.com>
2025-03-19 11:47:19 -0300

    implemented seller update

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== TEST 1: seller findById ===");
-		Seller seller = sellerDao.findById(3);
+		Seller seller = sellerDao.findById(1);
 		System.out.println(seller);
 
 		System.out.println("\n=== TEST 2: seller findByDepartment ===");
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Teste nome");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 
 	}
 
